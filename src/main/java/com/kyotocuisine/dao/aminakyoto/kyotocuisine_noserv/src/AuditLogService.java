b684e824d68312a5e0d443a7746fb4899300f9f9
dk2182 <dk2182@g.rit.edu>
2026-04-17 19:40:09 +0200

    leelee leeelee

--- src/main/java/com/kyotocuisine/dao/aminakyoto/kyotocuisine_noserv/src/AuditLogService.java
+++
@@ -1,9 +0,0 @@
-public class AuditLogService {
-    public static void log(int userId, String action, String entity, int entityId) {
-        try {
-            AuditLogDAO.log(userId, action, entity, entityId);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
